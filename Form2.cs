3b46bcd1f561ce63220e37fd49c7563f05d9de23
mahiro <65147170+mahiro0802@users.noreply.github.com>
2021-09-16 08:00:19 +0900

    Add files via upload

---
+++ Form2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Twitchコメビュ
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://twitchapps.com/tmi/");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.oauth = textBox1.Text;
+            Properties.Settings.Default.name = textBox2.Text;
+            Properties.Settings.Default.Save();
+            this.Close();
+        }
+    }
+}
